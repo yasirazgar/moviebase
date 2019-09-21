4ac0f3a9f9474c6706a089849dc3ae9b746d115c
yasirazgar <yasirazgar@gmail.com>
2019-09-21 23:29:35 +0530

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,95 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+password = 'PassworD@55'
+yasir, azgar, randy = User.create(
+  [
+    {name: 'Yasir', email: "yasir@risk.com", password: password},
+    {name: 'Azgar', email: "azgar@risk.com", password: password},
+    {name: 'Randy', email: "randy@risk.com", password: password}
+  ]
+)
+
+desc = Proc.new {|movie| "Description for movie #{movie}, its director, year of release and etc."}
+pulp_fiction, inception, dark_knight = Movie.create(
+  ['Pulp Fiction', 'Inception', 'Dark Knight'].map do |movie|
+    {title: movie, description: desc.call(movie), user: yasir}
+  end
+)
+Rating.create(
+  [
+    {user: yasir, movie: pulp_fiction, rating: 5},
+    {user: azgar, movie: pulp_fiction, rating: 4},
+    {user: randy, movie: pulp_fiction, rating: 4}
+  ]
+)
+Rating.create(
+  [
+    {user: yasir, movie: inception, rating: 5},
+    {user: azgar, movie: inception, rating: 4},
+    {user: randy, movie: inception, rating: 3}
+  ]
+)
+Rating.create(
+  [
+    {user: yasir, movie: dark_knight, rating: 5},
+    {user: azgar, movie: dark_knight, rating: 3},
+    {user: randy, movie: dark_knight, rating: 4}
+  ]
+)
+
+avengers, marvel, pool = Movie.create(
+  ['Avengers', 'Captain Marvel', 'Dead Pool'].map do |movie|
+    {title: movie, description: desc.call(movie), user: azgar}
+  end
+)
+Rating.create(
+  [
+    {user: yasir, movie: avengers, rating: 4},
+    {user: azgar, movie: avengers, rating: 3},
+    {user: randy, movie: avengers, rating: 5}
+  ]
+)
+Rating.create(
+  [
+    {user: yasir, movie: marvel, rating: 3},
+    {user: azgar, movie: marvel, rating: 2},
+    {user: randy, movie: marvel, rating: 4}
+  ]
+)
+Rating.create(
+  [
+    {user: yasir, movie: pool, rating: 4},
+    {user: azgar, movie: pool, rating: 3},
+    {user: randy, movie: pool, rating: 2}
+  ]
+)
+
+django, insomnia, interstellar = Movie.create(
+  ['Django', 'Insomnia', 'Interstellar'].map do |movie|
+    {title: movie, description: desc.call(movie), user: randy}
+  end
+)
+Rating.create(
+  [
+    {user: yasir, movie: django, rating: 4},
+    {user: azgar, movie: django, rating: 4},
+    {user: randy, movie: django, rating: 4}
+  ]
+)
+Rating.create(
+  [
+    {user: yasir, movie: insomnia, rating: 3},
+    {user: azgar, movie: insomnia, rating: 4},
+    {user: randy, movie: insomnia, rating: 3}
+  ]
+)
+Rating.create(
+  [
+    {user: yasir, movie: interstellar, rating: 5},
+    {user: azgar, movie: interstellar, rating: 3},
+    {user: randy, movie: interstellar, rating: 4}
+  ]
+)
+
