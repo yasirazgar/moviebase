353e4db67af16db63915b14ca72e26ddd5240daa
yasirazgar <yasirazgar@gmail.com>
2019-09-22 19:46:27 +0530

    configure production url and node version

--- app/javascript/apis/moviesRequest.js
+++ app/javascript/apis/moviesRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: (process.env.NODE_ENV !== 'production') ? 'http://localhost:3000' : 'https://movibase.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
     // 'X-CSRF-Token': document.getElementsByName('csrf-token')[0].content,
